e8a2a0abdb84101185661db78b2196dd8898dcdc
Cchaima <125662053+Cchaima@users.noreply.github.com>
2026-01-08 00:20:11 +0100

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ st.set_page_config(page_title="Bank Churn MLOps", page_icon="🏦", layout="wide
 BASE_URL = "http://localhost:8000"
 API_URL = "https://bank-churn-app.grayplant-cb43b6b5.germanywestcentral.azurecontainerapps.io"
 PREDICT_URL = f"{BASE_URL}/predict"
-DRIFT_URL = f"{BASE_URL}/drift/check"
+DRIFT_URL = f"{API_URL}/drift/check/"
 
 # Barre latérale pour la navigation
 st.sidebar.title("Navigation")
