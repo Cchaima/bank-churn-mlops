4994e2ec4779abda1c01c064def1bb0a2c837a72
Cchaima <chaimaahajri8@gmail.com>
2026-01-07 01:29:57 +0100

    Activation du CI/CD et correction start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-# Lancer l'API en arrière-plan
+# Lancer FastAPI en arrière-plan
 uvicorn app.main:app --host 0.0.0.0 --port 8000 &
 
-# Attendre que l'API soit prête
-sleep 5
+# Attendre 10 secondes que l'API soit prête
+sleep 10
 
-# Lancer l'interface Streamlit
-streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0 --server.enableCORS false --server.enableXsrfProtection false
+# Lancer Streamlit
+streamlit run streamlit_app.py --server.port 8501 --server.address 0.0.0.0
